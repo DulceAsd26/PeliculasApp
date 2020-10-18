a3da7a5f5709f84970767eb541d9c4c46f3727b0
DulceAsd26 <juarez.dulce.1jm@gmail.com>
2020-10-17 22:02:24 -0500

    Creacion de componentes

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home/home.component';
+import { PeliculaComponent } from './pelicula/pelicula.component';
+import { BuscarComponent } from './buscar/buscar.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [HomeComponent, PeliculaComponent, BuscarComponent],
   imports: [
     CommonModule
   ]
